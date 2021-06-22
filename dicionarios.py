b9866689e20370809474cc93d9cee46dfc5c8a73
RudhyDev <rudhy.pereira@gmail.com>
2021-06-21 22:53:51 -0300

    Aula de Dicionários

---
+++ dicionarios.py
@@ -0,0 +1,48 @@
+# vingadores = {
+#     'Chris Evans' : 'Capitão América',
+#     'Mark Ruffalo' : 'Hulk',
+#     'Tom Hiddleston' : 'Loki',
+#     'Chris Hemworth' : 'Thor',
+#     'Robert Downey Jr' : 'Homem de Ferro',
+#     'Scarlett Johansson' : 'Viúva Negra'
+# }
+
+# vingadores['homem-formiga'] = 'Paul Rudd'
+# print(vingadores)
+
+# continuar = 'S'
+# notas_alunos = {}
+# while continuar == 'S':
+#     aluno = input('Nome do Aluno: ')
+#     nota = int(input('Informe a nota do aluno: '))
+#     notas_alunos[aluno] = nota
+#     continuar = input('Deseja continuar? (S/N ').upper()
+# print(notas_alunos)
+
+
+
+# numeros = {}
+# while len(numeros)<6:
+#     numero = int(input('Digite um número: '))
+#     quadrado = numero**2
+#     numeros[numero] = quadrado
+    
+# print(numeros)
+
+
+# numeros = {}
+# for i in range(1,11):
+#     numeros[i] = i**2
+# print(numeros)
+
+
+#Exercício 2- Crie um programa, utilizando dicionário, que simule a baixa de estoque 
+# das vendas de um supermercado. Não esqueça de fazer as seguintes validações:​
+
+# Produto Indisponível​
+# Produto Inválido​
+# Quantidade solicitada não disponível ​
+
+# O programa deverá mostrar para o cliente a quantidade de itens comprados e o total.
+
+#Ex.: Feira = {'Tomate': 10, 'Abacate': 15, 'Cebola' : 0, 'Batata': 12}
