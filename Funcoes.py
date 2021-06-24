75d335a5f2ec0842bfb21ef4240658c13512c7ef
RudhyDev <rudhy.pereira@gmail.com>
2021-06-23 22:44:06 -0300

    Segunda função

--- Funcoes.py
+++ Funcoes.py
@@ -1,9 +1,9 @@
-def somar(numero1, numero2):
-    '''
-    Essa função recebe DOIS numeros e mostra a soma deles
-    '''
+# def somar(numero1, numero2):
+#     '''
+#     Essa funcao recebe DOIS numeros e mostra a soma deles
+#     '''
     
-    return numero1 + numero2
+#     return numero1 + numero2
 
 
 # a = input('Deseja somar 20 com 30 ? (s/n) ')
@@ -12,7 +12,18 @@ def somar(numero1, numero2):
 # else:
 #     print('Beijinhos Carol')
 
-num1 = int(input('Digite o primeiro número para somar: '))
-num2 = int(input('Digite o segundo número para somar: '))
+# num1 = int(input('Digite o primeiro número para somar: '))
+# num2 = int(input('Digite o segundo número para somar: '))
 
-print(f'A soma é: {somar(num1,num2)}')
+# print(f'A soma é: {somar(num1,num2)}')
+
+# help(somar)
+
+def area(b,h):
+    return b*h
+
+a = input('Gostaria de saber a área de um retângulo? ')
+if a == 's':
+    b = int(input('Informe a base: '))
+    h = int(input('Informe a altura: '))
+    print(f'A área é: {area(b,h)} u.m. ²')
